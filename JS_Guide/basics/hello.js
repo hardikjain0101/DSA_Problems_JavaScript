7c308eaca36a06894dc3626b7cd5f316758afd4f
ganeshpatil386386 <ganeshpatil386386@gmail.com>
2023-10-09 20:32:34 +0530

    variable js file added

---
+++ JS_Guide/basics/hello.js
@@ -0,0 +1,12 @@
+var id = 1;
+const name = "Ganesh";
+let address = "Block 7, India";
+const mobile_Number = 1234567891;
+
+/*
+
+var = function scope & value can be change 
+const = bloock scope & can't change value + initialize value is must 
+let = block scope & value can be change 
+
+*/
